39cb225870088ee10c483822c43c854f18433315
Dm&try <dmitrypanyukov@gmail.com>
2024-06-24 06:15:51 +0300

    И всё ещё в процессе.

--- src/Puffenduy.java
+++ src/Puffenduy.java
@@ -11,4 +11,24 @@ public class Puffenduy extends Hogwarts {
 
     }
 
+    public int getHardworking() {
+        return hardworking;
+    }
+    public void setHardworking(int hardworking) {
+        this.hardworking = hardworking;
+    }
+
+    public int getLoyal() {
+        return loyal;
+    }
+    public void setLoyal(int loyal) {
+        this.loyal = loyal;
+    }
+
+    public int getHonest() {
+        return honest;
+    }
+    public void setHonest(int honest) {
+        this.honest = honest;
+    }
 }
---
+++ src/Ravenclaw.java
@@ -0,0 +1,46 @@
+public class Ravenclaw extends Hogwarts {
+    private int smart;
+    private int wise;
+    private int witty;
+    private int creativity;
+
+    public Ravenclaw(int powerOfMagic, int transgressionDistance, int smart, int wise, int witty, int creativity) {
+        super(powerOfMagic, transgressionDistance);
+        this.smart = smart;
+        this.wise = wise;
+        this.witty = witty;
+        this.creativity = creativity;
+    }
+
+    public int getSmart() {
+        return smart;
+    }
+
+    public void setSmart(int smart) {
+        this.smart = smart;
+    }
+
+    public int getWise() {
+        return wise;
+    }
+
+    public void setWise(int wise) {
+        this.wise = wise;
+    }
+
+    public int getWitty() {
+        return witty;
+    }
+
+    public void setWitty(int witty) {
+        this.witty = witty;
+    }
+
+    public int getCreativity() {
+        return creativity;
+    }
+
+    public void setCreativity(int creativity) {
+        this.creativity = creativity;
+    }
+}
